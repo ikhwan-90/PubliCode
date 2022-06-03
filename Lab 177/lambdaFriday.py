869c3d52067d95b88f6d4ff9655e6000f673f59d
ikhwanAWS <104479843+ikhwanAWS@users.noreply.github.com>
2022-06-03 18:54:20 +0800

    Update lambdaFriday.py

--- Lab 177/lambdaFriday.py
+++ Lab 177/lambdaFriday.py
@@ -5,19 +5,18 @@ def lambda_handler(event, context):
     s3 = boto3.client('s3')
     
     if event:
-        print("Event : ", event)
+        #Fetch and open object
         file_obj = event["Records"][0]
         filename = str(file_obj['s3']['object']['key'])
-        print("Filename: ", filename)
-        
-        
-        obj = s3.get_object(Bucket='lab177060322', Key = filename)
+        obj = s3.get_object(Bucket='<bucket-name>', Key = filename)
         file_contents = obj["Body"].read().decode('utf-8')
         countWords = str(len(file_contents))
+        
+        #Display for test event
         result = print('The word count in the file ' + str(filename) + ' is ' + str(countWords))
         
         # Send message to SNS
-        SNS_ARN = 'arn:aws:sns:us-west-2:137906345418:lab177topic'
+        SNS_ARN = '<topic-ARN>'
         sns_client = boto3.client('sns')
         sns_client.publish(
             TopicArn = SNS_ARN,
