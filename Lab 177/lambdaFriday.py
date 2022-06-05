280436638cbe3a585d7e9bd491c7ce7de601cf64
ikhwanAWS <104479843+ikhwanAWS@users.noreply.github.com>
2022-06-06 00:28:31 +0800

    Update lambdaFriday.py

--- Lab 177/lambdaFriday.py
+++ Lab 177/lambdaFriday.py
@@ -7,12 +7,19 @@ def lambda_handler(event, context):
     if event:
         #Fetch and open object
         file_obj = event["Records"][0]
-        filename = str(file_obj['s3']['object']['key'])
+        
+        #Filter for filename with spaces
+        rawfilename = (str(file_obj['s3']['object']['key'])).split("+")     #Object key name is URL encoded. "file name.jpg" => file+name.jpg
+        filename = " ".join(rawfilename)
+        
+        #Open file with decoded key
         obj = s3.get_object(Bucket='<bucket-name>', Key = filename)
         file_contents = obj["Body"].read().decode('utf-8')
+        
+        #Count words in file regardless of white spaces
         countWords = str(len(file_contents))
         
-        #Display for test event
+        #* Optional - Display for test event
         result = print('The word count in the file ' + str(filename) + ' is ' + str(countWords))
         
         # Send message to SNS
